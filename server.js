26f906d97a45f56dae30c3c83168158c3a7fe1e1
lorenzo.poni09@gmail.com <lorenzo.poni09@gmail.com>
2026-07-06 12:28:57 +0200

    Aggiunti oggetti dinamici: menu da JSON, carrello, contatore vendite, orari apertura

--- server.js
+++ server.js
@@ -3,11 +3,28 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+let venditeTotali = 1523;
+
 app.use(express.static('public'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'data', 'menu.json'));
+});
+
+app.get('/api/vendite', (req, res) => {
+    res.json({ vendite: venditeTotali });
+});
+
+app.post('/api/vendite', (req, res) => {
+    const { quantita } = req.body;
+    venditeTotali += quantita;
+    res.json({ vendite: venditeTotali });
 });
 
 app.listen(PORT, () => {
